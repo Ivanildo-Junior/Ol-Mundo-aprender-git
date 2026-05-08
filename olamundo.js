67bb10cbe752469ff5bd74d91c6a12c2b0202f71
IVANILDO DE SANTANA JUNIOR <ivanildo.junior5868542@edu.pe.senac.br>
2026-05-07 21:26:45 -0300

    Resiltado da Soma

--- olamundo.js
+++ olamundo.js
@@ -4,3 +4,9 @@ console.log("Aprendendo git");
 
 let resultado = 5 + 5;
 console.log(resultado);
+
+function somar(a, b) {
+    return a + b;
+}
+let resultado = somar(5, 10);
+console.log(resultado);
